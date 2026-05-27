cdd079edb90d3d24a75b54363abe3754b11fab9e
Hansaja132 <isirahansajananayakkara@gmail.com>
2026-05-27 21:16:54 +0530

    Add Post model with fillable attributes and create posts migration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        if (in_array(env('APP_ENV'), ['local', 'testing', 'development'])) {
+            $middleware->validateCsrfTokens(except: [
+                'formsubmit',
+            ]);
+        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
